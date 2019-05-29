b93173bbb60e0a2cc69cc53a9bacabc460a3a2c1
Eduardo <spoky.br@gmail.com>
2019-05-29 15:58:22 -0300

    adding image link as doc ref

--- SqlBuilder/Program.cs
+++ SqlBuilder/Program.cs
@@ -13,6 +13,9 @@ namespace SqlBuilder
             // -> example of how to implement a LINQ provider:
             //      https://blogs.msdn.microsoft.com/mattwar/2008/11/18/linq-building-an-iqueryable-provider-series/
 
+            // -> Image of Expression:
+            //      http://ryanohs.com/wp-content/uploads/2016/04/ExpressionTree.png
+
             var users = new List<User> {
                 new User { Id = 1, AccessCount = 2, Birth = DateTime.Now.AddYears(-18), Name = "Toothless Dragon", Email = "tooth@less.co", Password="123" },
                 new User { Id = 2, AccessCount = 666, Birth = DateTime.Now.AddYears(-50), Name = "The Joker", Email = "joker@batman.com", Password="321" },
